7dd158bd2fb6d3397065eed294f6b58dfc1f926e
Jaikrishnan Arun <jaikrishnanarun@hotmail.com>
2020-05-22 15:21:22 -0700

    added rayon

--- src/main.rs
+++ src/main.rs
@@ -121,7 +121,6 @@ fn main() {
         let band_lower_right = pixel_to_point(bounds, (bounds.0, top + 1), upper_left, lower_right);
         render(band, band_bounds, band_upper_left, band_lower_right);
     });
-
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
 
